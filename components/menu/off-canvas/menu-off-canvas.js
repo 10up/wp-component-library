2847ff48ea2e2b9fbefc741bfc3aa5e7cb00db22
Dominic Magnifico <dominic.magnifico@10up.com>
2016-08-24 12:56:42 -0600

    Wrap js to prevent leaking into the global scope.

--- components/menu/off-canvas/menu-off-canvas.js
+++ components/menu/off-canvas/menu-off-canvas.js
@@ -1,51 +1,58 @@
-var subMenuItem = document.querySelectorAll( '.sub-menu .menu-item' ),
-    subMenu     = document.querySelectorAll( '.sub-menu' );
+;(function (w, doc) {
 
-document.addEventListener( 'focus', focusToggle, true );
+  // Enable strict mode
+  "use strict";
 
-function focusToggle( e ) {
-  if ( e.target.classList.contains( 'menu-level-1' ) ) {
-    subMenu[0].classList.add( 'hover' );
-  } else {
-    subMenu[0].classList.remove( 'hover' );
-  }
-}
+  var subMenuItem = document.querySelectorAll( '.sub-menu .menu-item' ),
+  subMenu     = document.querySelectorAll( '.sub-menu' );
 
-/**
- * Adds helper function for devs to plug in basic values to assist in off canvas functionality.
- * @param {Element} toggler         //The class name of the element that toggles the off canvas menu.
- * @param {Element} primaryMenu     //The class name of the element that is the primary menu.
- * @param {Element} primaryMenuWrap //The wrapper that contains the primary menu.
- * @param {String} direction        //The direction that the off canvas menu should fly in from.
- */
-function offCanvas( toggler, primaryMenu, primaryMenuWrap, direction ) {
-  var body       = document.body,
-      menuToggle = document.querySelector( toggler ),
-      menu       = document.querySelector( primaryMenu ),
-      wrap       = document.querySelector( primaryMenuWrap ),
-      dir        = direction;
+  document.addEventListener( 'focus', focusToggle, true );
 
-  menuToggle.onclick = function() {
-    menu.classList.toggle( 'is-active' );
-    body.classList.toggle( 'is-active-off-canvas' );
-
-    if( body.classList.contains( 'is-active-off-canvas' ) ) {
-      var menuWidth = menu.offsetWidth,
-          bodyDir   = dir,
-          wrapDir   = dir,
-          bodyDir   = ( bodyDir === 'left' ) ? body.style.left = menuWidth + 'px' : body.style.right = menuWidth + 'px',
-          wrapDir   = ( wrapDir === 'left' ) ? wrap.style.left = -menuWidth + 'px' : wrap.style.right = -menuWidth + 'px';
-
-      wrap.classList.add( dir );
+  function focusToggle( e ) {
+    if ( e.target.classList.contains( 'menu-level-1' ) ) {
+      subMenu[0].classList.add( 'hover' );
     } else {
-      var bodyDir   = dir,
-          wrapDir   = dir,
-          bodyDir   = ( bodyDir === 'left' ) ? body.style.left = '0' : body.style.right = '0',
-          wrapDir   = ( wrapDir === 'left' ) ? wrap.style.left = '0' : wrap.style.right = '0';
-
-      wrap.classList.remove( dir );
+      subMenu[0].classList.remove( 'hover' );
     }
   }
-}
 
-offCanvas( '.site-menu-toggle', '.primary-menu', '.site-header', 'right' );
+  /**
+  * Adds helper function for devs to plug in basic values to assist in off canvas functionality.
+  * @param {Element} toggler         //The class name of the element that toggles the off canvas menu.
+  * @param {Element} primaryMenu     //The class name of the element that is the primary menu.
+  * @param {Element} primaryMenuWrap //The wrapper that contains the primary menu.
+  * @param {String} direction        //The direction that the off canvas menu should fly in from.
+  */
+  function offCanvas( toggler, primaryMenu, primaryMenuWrap, direction ) {
+    var body       = document.body,
+    menuToggle = document.querySelector( toggler ),
+    menu       = document.querySelector( primaryMenu ),
+    wrap       = document.querySelector( primaryMenuWrap ),
+    dir        = direction;
+
+    menuToggle.onclick = function() {
+      menu.classList.toggle( 'is-active' );
+      body.classList.toggle( 'is-active-off-canvas' );
+
+      if( body.classList.contains( 'is-active-off-canvas' ) ) {
+        var menuWidth = menu.offsetWidth,
+            bodyDir   = dir,
+            wrapDir   = dir,
+            bodyDir   = ( bodyDir === 'left' ) ? body.style.left = menuWidth + 'px' : body.style.right = menuWidth + 'px',
+            wrapDir   = ( wrapDir === 'left' ) ? wrap.style.left = -menuWidth + 'px' : wrap.style.right = -menuWidth + 'px';
+
+        wrap.classList.add( dir );
+      } else {
+        var bodyDir   = dir,
+            wrapDir   = dir,
+            bodyDir   = ( bodyDir === 'left' ) ? body.style.left = '0' : body.style.right = '0',
+            wrapDir   = ( wrapDir === 'left' ) ? wrap.style.left = '0' : wrap.style.right = '0';
+
+        wrap.classList.remove( dir );
+      }
+    }
+  }
+
+  offCanvas( '.site-menu-toggle', '.primary-menu', '.site-header', 'left' );
+
+} )( this, this.document );
