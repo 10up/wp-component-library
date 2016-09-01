3b909f002cc9cb24e12c1df6fb2c6339891e4fbe
Tim Wright <timwright12@gmail.com>
2016-09-01 09:43:37 -0400

    merge with bugfix/media-queries

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -2,7 +2,7 @@
   Name: Accessible Modal Plugin
   Contributors: Tim Wright (tim@10up.com, @csskarma)
   License: MIT, https://opensource.org/licenses/MIT
-  Converted from a jQuery plugin originally written by @scottohara via Smashing Magazine
+  Converted from a jQuery plugin originally written by @scottohara: https://github.com/scottaohara/accessible-components
 */
 
 ;(function (w, doc) {
