31a52472110044d33cc489ebc7ee053ec4e955d0
Tim Wright <timwright12@gmail.com>
2016-09-06 15:04:35 -0400

    removed classList from tab component

--- _component/tabs/component.js
+++ _component/tabs/component.js
@@ -3,6 +3,50 @@
     // Enable strict mode
     'use strict';
 
+	// Local object for method references
+	var TenUp_Tabs = {};
+
+	/*
+		Cross-browser way to deal with class management
+	*/
+
+	TenUp_Tabs.hasClass = function ( el, cls ) {
+		if (el.classList) {
+		  return el.classList.contains(cls);
+		} else {
+		  return !!el.cls.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+		}
+	};
+
+	/*
+		Cross-browser way to add a class
+	*/
+
+	TenUp_Tabs.addClass = function ( el, cls ) {
+		if ( el.classList ) {
+		  el.classList.add( cls );
+		} else if( !TenUp_Tabs.hasClass( el, cls ) ) {
+		  el.cls += " " + cls;
+		}
+	};
+
+	/*
+		Cross-browser way to remove a class
+	*/
+
+	TenUp_Tabs.removeClass = function ( el, cls ) {
+		if ( el.classList ) {
+		  el.classList.remove( cls );
+		} else if( TenUp_Tabs.hasClass( el, cls ) ) {
+		  var reg = new RegExp( '(\\s|^)' + cls + '(\\s|$)' );
+		  el.cls = el.cls.replace( reg, ' ' );
+		}
+	};
+
+	/*
+		Start Component
+	*/
+
     var tabs = doc.querySelectorAll( '.tabs' );
 
     var forEach = function ( array, callback, scope ) {
@@ -19,15 +63,15 @@
 
       // Set state for the first .tab-content item
       tabContent[0].setAttribute( 'aria-hidden', 'false' );
-      tabContent[0].classList.add( 'is-active' );
+      TenUp_Tabs.addClass( tabContent[0], 'is-active' );
 
       // Set state for the first .tab-item
       var firstTab = tabs[index].querySelectorAll( '.tab-item' );
-      firstTab[0].classList.add( 'is-active' );
+      TenUp_Tabs.addClass( firstTab[0], 'is-active' );
 
       forEach( tabLinks, function( index, value ) {
-        var tab = value,
-            tabId = 'tab-' + tab.getAttribute( 'href' ).slice( 1 );
+        var tab = value;
+        var tabId = 'tab-' + tab.getAttribute( 'href' ).slice( 1 );
 
         // Set ARIA and ID attributes
         tab.setAttribute( 'id', tabId );
@@ -42,34 +86,40 @@
           event.preventDefault();
 
           // Handle opening and closing of the tabs on mobile devices
-          if( tab.parentNode.classList.contains( 'is-active') ) {
-            tab.parentNode.parentNode.classList.toggle( 'm-is-active' );
+          if ( TenUp_Tabs.hasClass( tab.parentNode, 'is-active' ) ) {
+
+	        if ( TenUp_Tabs.hasClass( tab.parentNode.parentNode, 'm-is-active' ) ) {
+		        TenUp_Tabs.removeClass( tab.parentNode.parentNode, 'm-is-active' );
+	        } else {
+		        TenUp_Tabs.addClass( tab.parentNode.parentNode, 'm-is-active' );
+	        }
+
           } else {
-            tab.parentNode.parentNode.classList.remove( 'm-is-active' );
+            TenUp_Tabs.removeClass( tab.parentNode.parentNode, 'm-is-active' );
           }
 
           // Change state of previously selected activeTab item
           forEach( activeTab, function( index, value ) {
-            if( value.classList.contains( 'is-active') ) {
-              value.classList.remove( 'is-active' );
+	        if ( TenUp_Tabs.hasClass( value, 'is-active' ) ) {
+              TenUp_Tabs.removeClass( value, 'is-active' );
               tabLinks[index].setAttribute( 'aria-selected', 'false' );
             }
           });
 
           // Set state of newly selected tab list item
           tab.setAttribute( 'aria-selected', 'true' );
-          tab.parentNode.classList.add( 'is-active' );
+          TenUp_Tabs.addClass( tab.parentNode, 'is-active' );
 
           // Change state of previously selected tabContent item
           forEach( tabContent, function( index, value ) {
-            if( value.classList.contains( 'is-active' ) ) {
-              value.classList.remove( 'is-active' );
+	        if ( TenUp_Tabs.hasClass( value, 'is-active' ) ) {
+              TenUp_Tabs.removeClass( value, 'is-active' );
               tabContent[index].setAttribute( 'aria-hidden', 'true' );
             }
           });
 
           // Show newly selected content
-          tabContent[index].classList.add( 'is-active' );
+          TenUp_Tabs.addClass( tabContent[index], 'is-active' );
           tabContent[index].setAttribute( 'aria-hidden', 'false' );
 
           // Set focus to the first heading in the newly revealed tab content
