5d7e7054da404e91b5ecf8e155b8181e684165a2
Tim Wright <timwright12@gmail.com>
2016-09-06 14:01:01 -0400

    removed classList from dropdown-menu

--- _component/menu-dropdown/component.js
+++ _component/menu-dropdown/component.js
@@ -3,24 +3,74 @@
   // Enable strict mode
   "use strict";
 
-  var subMenuItem = document.querySelectorAll( '.sub-menu .menu-item' ),
-  subMenu     = document.querySelectorAll( '.sub-menu' );
+  // Local object for method references
+  var TenUp_Menu_Dropdown = {};
 
-  document.addEventListener( 'focus', focusToggle, true );
+  /*
+    Cross-browser way to deal with class management
+  */
+
+  TenUp_Menu_Dropdown.hasClass = function ( el, cls ) {
+    if (el.classList) {
+      return el.classList.contains(cls);
+    } else {
+      return !!el.cls.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+    }
+  };
+
+  /*
+    Cross-browser way to add a class
+  */
+
+  TenUp_Menu_Dropdown.addClass = function ( el, cls ) {
+    if ( el.classList ) {
+      el.classList.add( cls );
+    } else if( !TenUp_Menu_Dropdown.hasClass( el, cls ) ) {
+      el.cls += " " + cls;
+    }
+  };
+
+  /*
+    Cross-browser way to remove a class
+  */
+
+  TenUp_Menu_Dropdown.removeClass = function ( el, cls ) {
+    if ( el.classList ) {
+      el.classList.remove( cls );
+    } else if( TenUp_Menu_Dropdown.hasClass( el, cls ) ) {
+      var reg = new RegExp( '(\\s|^)' + cls + '(\\s|$)' );
+      el.cls = el.cls.replace( reg, ' ' );
+    }
+  };
+
+  /*
+	  Start Component
+  */
+
+  var subMenuItem = doc.querySelectorAll( '.sub-menu .menu-item' );
+  var subMenu = doc.querySelectorAll( '.sub-menu' );
+  var menuToggle = document.querySelector( '.site-menu-toggle' );
+  var menu = doc.querySelector( '.primary-menu' );
+
+  doc.addEventListener( 'focus', focusToggle, true );
 
   function focusToggle( e ) {
-    if ( e.target.classList.contains( 'menu-level-1') ) {
-      subMenu[0].classList.add( 'hover' );
+
+	if( TenUp_Menu_Dropdown.hasClass( e.target, 'menu-level-1' ) ) {
+      TenUp_Menu_Dropdown.addClass( subMenu[0], 'hover' );
     } else {
-      subMenu[0].classList.remove( 'hover' );
+      TenUp_Menu_Dropdown.removeClass( subMenu[0], 'hover' );
     }
+
   }
 
-  var menuToggle = document.querySelector( '.site-menu-toggle' ),
-  menu       = document.querySelector( '.primary-menu' );
-
   menuToggle.onclick = function() {
-    menu.classList.toggle( 'is-active' );
+
+	if( TenUp_Menu_Dropdown.hasClass( menu, 'is-active' ) ) {
+      TenUp_Menu_Dropdown.removeClass( menu, 'is-active' );
+    } else {
+      TenUp_Menu_Dropdown.addClass( menu, 'is-active' );
+    }
 
     // Update the menu state within the button
     if( this.getAttribute( 'aria-expanded') === 'false' ) {
