782801c531a0cf955192488d6244e485b4c3bfdc
Taylor Lovett <tlovett88@gmail.com>
2016-09-14 14:34:35 -0400

    Simply code

--- _component/menu-dropdown/component.js
+++ _component/menu-dropdown/component.js
@@ -1,87 +1,27 @@
-;(function (w, doc) {
-
-	// Polyfill for el.matches
-	if (!Element.prototype.matches) {
-		Element.prototype.matches =
-		Element.prototype.matchesSelector ||
-		Element.prototype.mozMatchesSelector ||
-		Element.prototype.msMatchesSelector ||
-		Element.prototype.oMatchesSelector ||
-		Element.prototype.webkitMatchesSelector ||
-		function(s) {
-			var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-			i = matches.length;
-			while (--i >= 0 && matches.item(i) !== this) {}
-			return i > -1;
-		};
-	}
-
-	// Enable strict mode
-	"use strict";
-
-	// Local object for method references
-	var TenUp_Menu_Dropdown = {};
-
-	/*
-	 * Cross-browser way to deal with class management
-	 */
-
-	TenUp_Menu_Dropdown.hasClass = function ( el, cls ) {
-		return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test( el.className );
-	};
-
-	/*
-	 * Cross-browser way to add a class
-	 */
-
-	TenUp_Menu_Dropdown.addClass = function ( el, cls ) {
-		if ( el.classList ) {
-			el.classList.add(cls);
-		} else if (!TenUp_Menu_Dropdown.hasClass(el, cls)) {
-		el.className += " " + cls;
-	}
-	};
-
-	/*
-	 * Cross-browser way to remove a class
-	 */
-
-	TenUp_Menu_Dropdown.removeClass = function ( el, cls ) {
-		if ( el.classList ) {
-			el.classList.remove( cls );
-		} else if( TenUp_Menu_Dropdown.hasClass( el, cls ) ) {
-			var reg = new RegExp( '(\\s|^)' + cls + '(\\s|$)' );
-			el.className = el.className.replace( reg, ' ' );
-		}
-	};
+( function() {
+	'use strict';
 
 	/*
 	 * Start Component
 	 */
 
-	var subMenuItem = doc.querySelectorAll( '.sub-menu .menu-item' );
-	var subMenu = doc.querySelectorAll( '.sub-menu' );
-	var menuToggle = document.querySelector( '.site-menu-toggle' );
-	var menu = doc.querySelector( '.primary-menu' );
+	var menu = document.querySelector( '.primary-menu' );
 
-	doc.addEventListener( 'focus', focusToggle, true );
-
-	function focusToggle( e ) {
-
-	if( TenUp_Menu_Dropdown.hasClass( e.target, 'menu-level-1' ) ) {
-			TenUp_Menu_Dropdown.addClass( subMenu[0], 'hover' );
-		} else {
-			TenUp_Menu_Dropdown.removeClass( subMenu[0], 'hover' );
-		}
-
+	if ( 1 > menu.length ) {
+		// No menu found
+		return;
 	}
+
+	var subMenuItem = menu.querySelectorAll( '.sub-menu .menu-item' ),
+		subMenu = menu.querySelectorAll( '.sub-menu' ),
+		menuToggle = document.querySelector( '.site-menu-toggle' );
 
 	menuToggle.onclick = function() {
 
-	if( TenUp_Menu_Dropdown.hasClass( menu, 'is-active' ) ) {
-			TenUp_Menu_Dropdown.removeClass( menu, 'is-active' );
+		if ( menu.className.match( 'is-active' ) ) {
+			menu.className = menu.className.replace( / ?is-active/, '' );
 		} else {
-			TenUp_Menu_Dropdown.addClass( menu, 'is-active' );
+			menu.className = menu.className.replace( / ?is-active/, '' ) + ' is-active';
 		}
 
 		// Update the menu state within the button
@@ -92,8 +32,7 @@
 		}
 
 		// Set focus to the first item in the menu
-		menu.querySelectorAll('a')[0].focus();
+		menu.querySelectorAll( 'a' )[0].focus();
+	};
 
-	}
-
-} )( this, this.document );
+} )();
