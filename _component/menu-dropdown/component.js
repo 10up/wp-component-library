119bb6ce8f44259f50f725ce474500775c5261d7
Tim Wright <timwright12@gmail.com>
2016-09-23 11:49:00 -0400

    added partial submenu functionality before my brain started to melt

--- _component/menu-dropdown/component.js
+++ _component/menu-dropdown/component.js
@@ -3,8 +3,6 @@
 		- Large screen: keep menus open on child focus
 		- Large screen: Install click to open option if attribute is set
 		- Small screen: Install click to open option globally
-		- Add has-popup to submenus
-		- init aria on sub menus
 ********************************/
 
 ( function() {
@@ -24,6 +22,9 @@
 	var sub_menu_acion = menu_toggle.getAttribute( 'data-submenu' );
 	var menu_items_with_children = menu.querySelectorAll('.menu-item-has-children');
 	var menu_items_with_children_count = menu_items_with_children.length;
+	var currentTarget;
+	var target
+	var i;
 
 	/*
 		Helper functions
@@ -96,6 +97,15 @@
 
 	};
 
+	// Listener for submenu on click
+	var listener_submenu_click = function( e ) {
+
+		e.preventDefault();
+
+		console.log( 'sub menu' );
+
+	};
+
 	// Listener for the window resize
 	var listener_window = debounce( function( e ) {
 
@@ -110,23 +120,52 @@
 	// Method to create the small screen menu
 	var menu_create = function() {
 
-		if( !document.body.classList.contains('menu-created') ) {
+		if( !document.body.classList.contains( 'menu-created' ) ) {
 
-			if( !document.body.classList.contains('menu-is-open') ) {
-				menu.setAttribute('aria-hidden', 'true');
-				menu_toggle.setAttribute('aria-expanded', 'false');
+			if( !document.body.classList.contains( 'menu-is-open' ) ) {
+				menu.setAttribute( 'aria-hidden', 'true' );
+				menu_toggle.setAttribute( 'aria-expanded', 'false' );
 			} else {
-				menu.setAttribute('aria-hidden', 'false');
-				menu_toggle.setAttribute('aria-expanded', 'true');
+				menu.setAttribute( 'aria-hidden', 'false' );
+				menu_toggle.setAttribute( 'aria-expanded', 'true' );
 			}
 
 			// Bind the event
 			menu_toggle.addEventListener( 'click', listener_menu );
 
 			// Add the body class to prevent this from running again
-			document.body.classList.add('menu-created');
-			document.body.classList.remove('menu-destroyed');
+			document.body.classList.add( 'menu-created' );
+			document.body.classList.remove( 'menu-destroyed' );
 
+		}
+
+	};
+
+	// Method to fire when a user is tabbing through a menu
+	var listener_focusin = function( e ) {
+
+		var first_link = e.currentTarget.querySelector('a');
+		currentTarget = e.currentTarget;
+		target = e.target;
+
+		if( currentTarget.contains( target ) ) {
+
+			currentTarget.classList.add( 'child-has-focus' );
+			currentTarget.querySelector('.sub-menu').setAttribute( 'aria-hidden', 'false' );
+
+		}
+
+	};
+
+	// Method to fire when a user tabs out of a menu
+	var listener_focusout = function( e ) {
+
+		currentTarget = e.currentTarget;
+		target = e.target;
+
+		if( !currentTarget.contains( target ) ) {
+			currentTarget.classList.remove( 'child-has-focus' );
+			currentTarget.querySelector('.sub-menu').setAttribute( 'aria-hidden', 'true' );
 		}
 
 	};
@@ -134,17 +173,17 @@
 	// Method to destroy the small screen menu
 	var menu_destroy = function() {
 
-		if( !document.body.classList.contains('menu-destoryed') ) {
+		if( !document.body.classList.contains( 'menu-destoryed' ) ) {
 
 			// Remove aria-hidden, because we don't need it.
-			menu.removeAttribute('aria-hidden');
+			menu.removeAttribute( 'aria-hidden' );
 
 			// Unbind the event
 			menu_toggle.removeEventListener( 'click', listener_menu );
 
 			// Add the body class to prevent this from running again
-			document.body.classList.add('menu-destroyed');
-			document.body.classList.remove('menu-created');
+			document.body.classList.add( 'menu-destroyed' );
+			document.body.classList.remove( 'menu-created' );
 
 		}
 
@@ -166,9 +205,41 @@
 	}
 
 	/*
-		Resize event
+		Events
 	*/
 
+	// Debounced resize event to create and destroy the small screen menu options
 	window.addEventListener( 'resize', listener_window );
+
+	// Loop through all items with sub menus and bind focus to them for tabbing
+	for ( i = 0; i < menu_items_with_children_count; i = i + 1 ) {
+
+		// Let a screen reader know this menu has a submenu by hooking into the first link
+		menu_items_with_children[i].querySelector('a').setAttribute( 'aria-haspopup', 'true' );
+
+		// Hide and label each sub menu
+		menu_items_with_children[i].querySelector('.sub-menu').setAttribute( 'aria-hidden', 'true' );
+		menu_items_with_children[i].querySelector('.sub-menu').setAttribute( 'aria-label', 'Submenu' );
+
+		// Bind events, using focusin and focusout for greater browser support with focus event delegation
+		menu_items_with_children[i].addEventListener( 'focusin', listener_focusin );
+		menu_items_with_children[i].addEventListener( 'focusout', listener_focusout );
+
+		// If the screen is small or the action is set to click
+		if( get_screen_size( 'small' ) || sub_menu_acion === 'click' ) {
+			menu_items_with_children[i].addEventListener( 'click', listener_submenu_click );
+		}
+
+		// Unbind the click action for submenus on large screen if the action should be hover
+		// Make sure this doesn't permanently live in the loop
+		window.addEventListener( 'resize', function() {
+
+			if( sub_menu_acion !== 'click' ) {
+				menu_items_with_children[i].removeEventListener( 'click', listener_submenu_click );
+			}
+
+		} );
+
+	} // for()
 
 } )();
