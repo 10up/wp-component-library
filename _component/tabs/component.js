4e10d21ebd99ec4f81dc53ef75498de598fe6dad
Taylor Lovett <tlovett88@gmail.com>
2016-10-04 13:38:18 -0400

    Api-ify tabs

--- _component/tabs/component.js
+++ _component/tabs/component.js
@@ -1,85 +1,116 @@
+/********************************
+
+	Name: WordPress Accessible Tabs
+	Usage:
+
+	TenUp.tabs({
+		'target': '.tabs', // ID (or class) of the tab container
+	}, function() {
+		console.log('Amazing callback function!');
+	});
+
+********************************/
+
 ( function() {
 
 	'use strict';
 
-	var tabs = document.querySelectorAll( '.tabs' );
+	// Define global TenUp object if it doesn't exist
+	if ( 'object' !== typeof window.TenUp ) {
+		window.TenUp = {};
+	}
 
-	var forEach = function( array, callback, scope ) {
-		for ( var i = 0; i < array.length; i++ ) {
-			callback.call( scope, i, array[i] ); // passes back stuff we need
+	window.TenUp.tabs = function( options, callback ) {
+
+		if ( 'undefined' === typeof options.target ) {
+			return false;
 		}
-	};
 
-	forEach( tabs, function( index, value ) {
-		var tabContent   = tabs[index].querySelectorAll( '.tab-content' ),
-			tabsList     = tabs[index].querySelectorAll( '.tab-list' ),
-			tabLinks     = tabs[index].querySelectorAll( '.tab-list li > a' ),
-			activeTab    = tabs[index].querySelectorAll( '.tab-list li' );
+		var tabs = document.querySelectorAll( options.target );
 
-		// Set state for the first .tab-item
-		var firstTab = tabs[index].querySelectorAll( '.tab-item' );
-		firstTab[0].classList.add( 'is-active' );
+		// Simple forEach to make iteration easier
+		var forEach = function( array, callback, scope ) {
+			for ( var i = 0; i < array.length; i++ ) {
+				callback.call( scope, i, array[i] ); // passes back stuff we need
+			}
+		};
 
-		forEach( tabLinks, function( index, value ) {
-			var tab = value;
-			var tabId = 'tab-' + tab.getAttribute( 'href' ).slice( 1 );
+		forEach( tabs, function( index, value ) {
+			var tabContent   = tabs[index].querySelectorAll( '.tab-content' ),
+				tabsList     = tabs[index].querySelectorAll( '.tab-list' ),
+				tabLinks     = tabs[index].querySelectorAll( '.tab-list li > a' ),
+				activeTab    = tabs[index].querySelectorAll( '.tab-list li' );
 
-			// Set ARIA and ID attributes
-			tab.setAttribute( 'id', tabId );
-			tab.setAttribute( 'aria-selected', false );
-			tab.parentNode.setAttribute( 'role', 'presentation' );
-			tabContent[index].setAttribute( 'aria-labelledby', tabId );
-			tabContent[index].setAttribute( 'aria-hidden', true );
+			// Set state for the first .tab-item
+			var firstTab = tabs[index].querySelectorAll( '.tab-item' );
+			firstTab[0].classList.add( 'is-active' );
 
-			tab.onclick = tabHandle;
+			forEach( tabLinks, function( index, value ) {
+				var tab = value;
+				var tabId = 'tab-' + tab.getAttribute( 'href' ).slice( 1 );
 
-			function tabHandle( event ) {
-				event.preventDefault();
+				// Set ARIA and ID attributes
+				tab.setAttribute( 'id', tabId );
+				tab.setAttribute( 'aria-selected', false );
+				tab.parentNode.setAttribute( 'role', 'presentation' );
+				tabContent[index].setAttribute( 'aria-labelledby', tabId );
+				tabContent[index].setAttribute( 'aria-hidden', true );
 
-				// Handle opening and closing of the tabs on mobile devices
-				if ( tab.parentNode.classList.contains( 'is-active' ) ) {
-					tab.parentNode.parentNode.classList.toggle( 'm-is-active' );
-				} else {
-					tab.parentNode.parentNode.classList.remove( 'm-is-active' );
+				tab.onclick = tabHandle;
+
+				function tabHandle( event ) {
+					event.preventDefault();
+
+					// Handle opening and closing of the tabs on mobile devices
+					if ( tab.parentNode.classList.contains( 'is-active' ) ) {
+						tab.parentNode.parentNode.classList.toggle( 'm-is-active' );
+					} else {
+						tab.parentNode.parentNode.classList.remove( 'm-is-active' );
+					}
+
+					// Change state of previously selected activeTab item
+					forEach( activeTab, function( index, value ) {
+
+						if ( value.classList.contains( 'is-active' ) ) {
+							value.classList.remove( 'is-active' );
+							tabLinks[index].setAttribute( 'aria-selected', 'false' );
+						}
+
+					} );
+
+					// Set state of newly selected tab list item
+					tab.setAttribute( 'aria-selected', 'true' );
+					tab.parentNode.classList.add( 'is-active' );
+
+					// Change state of previously selected tabContent item
+					forEach( tabContent, function( index, value ) {
+						if ( value.classList.contains( 'is-active' ) ) {
+							value.classList.remove( 'is-active' );
+							tabContent[index].setAttribute( 'aria-hidden', 'true' );
+						}
+					} );
+
+					// Show newly selected content
+					tabContent[index].classList.add( 'is-active' );
+					tabContent[index].setAttribute( 'aria-hidden', 'false' );
+
+					// Set focus to the first heading in the newly revealed tab content
+					tabContent[index].querySelector( 'h2' ).setAttribute( 'tabindex', -1 );
+					tabContent[index].querySelector( 'h2' ).focus();
+
 				}
 
-				// Change state of previously selected activeTab item
-				forEach( activeTab, function( index, value ) {
+				// Set state for the first .tab-content item
+				tabContent[0].classList.add( 'is-active' );
+				tabContent[0].setAttribute( 'aria-hidden', 'false' );
 
-					if ( value.classList.contains( 'is-active' ) ) {
-						value.classList.remove( 'is-active' );
-						tabLinks[index].setAttribute( 'aria-selected', 'false' );
-					}
-
-				} );
-
-				// Set state of newly selected tab list item
-				tab.setAttribute( 'aria-selected', 'true' );
-				tab.parentNode.classList.add( 'is-active' );
-
-				// Change state of previously selected tabContent item
-				forEach( tabContent, function( index, value ) {
-					if ( value.classList.contains( 'is-active' ) ) {
-						value.classList.remove( 'is-active' );
-						tabContent[index].setAttribute( 'aria-hidden', 'true' );
-					}
-				} );
-
-				// Show newly selected content
-				tabContent[index].classList.add( 'is-active' );
-				tabContent[index].setAttribute( 'aria-hidden', 'false' );
-
-				// Set focus to the first heading in the newly revealed tab content
-				tabContent[index].querySelector( 'h2' ).setAttribute( 'tabindex', -1 );
-				tabContent[index].querySelector( 'h2' ).focus();
-
-			}
-
-			// Set state for the first .tab-content item
-			tabContent[0].classList.add( 'is-active' );
-			tabContent[0].setAttribute( 'aria-hidden', 'false' );
-
+			} );
 		} );
-	} );
+
+		// Execute the callback function
+		if( typeof callback === 'function' ) {
+			callback.call();
+		}
+	};
 
 } )();
