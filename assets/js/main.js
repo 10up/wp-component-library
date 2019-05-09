127e6da256c5c54d5bbfb85338847881bc278e30
Karine <karine.do@get10up.com>
2019-05-09 10:54:45 -0400

    remove custom tabs code

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,32 +152,9 @@
 			return;
 		}
 
-		var tabList = document.querySelectorAll( '.code-tab-list a' );
-
-		for ( var i = 0; i < tabList.length; i++ ) {
-			tabList[i].addEventListener( 'click', function(e) {
-				e.preventDefault();
-
-				var templated     = document.getElementById( 'templated' ),
-					templatedLink = document.getElementById( 'js-templated-link' ),
-					text          = document.getElementById( 'text' ),
-					textLink      = document.getElementById( 'js-text-link' );
-
-				if ( '#text' === this.getAttribute( 'href' ) ) {
-					text.classList.add( 'show' );
-					textLink.classList.add( 'show' );
-
-					templated.classList.remove( 'show' );
-					templatedLink.classList.remove( 'show' );
-				} else {
-					templated.classList.add( 'show' );
-					templatedLink.classList.add( 'show' );
-
-					text.classList.remove( 'show' );
-					textLink.classList.remove( 'show' );
-				}
-			});
-		}
+		let codeTabs = new TenUp.tabs( '.tabs', {
+			orientation: 'horizontal'
+		} );
 	}
 
   // Start the application
